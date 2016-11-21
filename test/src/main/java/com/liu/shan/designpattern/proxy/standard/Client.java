e5c818caeb6b6463c79ed8421d07cb82d285edce
wff <1024904798@qq.com>
2016-11-21 16:05:33 +0800

    update

--- test/src/main/java/com/liu/shan/designpattern/proxy/standard/Client.java
+++ test/src/main/java/com/liu/shan/designpattern/proxy/standard/Client.java
@@ -10,7 +10,7 @@ public class Client {
     public static void main(String[] args) {
         Subject subject = new RealSubject();
         InvocationHandler handler = new MyInvocationHandler(subject);
-        Subject proxy = (Subject) Proxy.newProxyInstance(handler.getClass().getClassLoader(),subject.getClass().getInterfaces(),handler);
+        Subject proxy = (Subject) Proxy.newProxyInstance(subject.getClass().getClassLoader(),subject.getClass().getInterfaces(),handler);
         proxy.doSomething();
     }
 }
